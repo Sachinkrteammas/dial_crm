0e8fa8d449b1d3649c60693567c6d70196490fd0
sachin <sachin.kumar@teammas.co.in>
2025-12-23 19:38:39 +0530

    sheduler change

--- crmapp/scheduler.py
+++ crmapp/scheduler.py
@@ -6,6 +6,6 @@ def start_scheduler():
     scheduler = BackgroundScheduler()
     # Run every hour
     # scheduler.add_job(fetch_lead_status_job, 'interval', hours=1, id='fetch_lead_status')
-    scheduler.add_job(fetch_lead_status_job, 'interval', minutes=1, id='fetch_lead_status', replace_existing=True)
+    scheduler.add_job(fetch_lead_status_job, 'interval', minutes=10, id='fetch_lead_status', replace_existing=True)
     scheduler.start()
     print("✅ APScheduler started successfully")
