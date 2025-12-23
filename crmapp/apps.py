de39b4ea4a36009d7a8904f7a98820937e99437d
sachin <sachin.kumar@teammas.co.in>
2025-12-23 19:57:02 +0530

    sheduler change

--- crmapp/apps.py
+++ crmapp/apps.py
@@ -1,13 +1,13 @@
-from django.apps import AppConfig
-
-
-class CrmappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crmapp'
-
-    def ready(self):
-        # Start scheduler only when server starts (not during migrations)
-        from django.conf import settings
-        if settings.DEBUG:
-            from .scheduler import start_scheduler
-            start_scheduler()
+# from django.apps import AppConfig
+#
+#
+# class CrmappConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'crmapp'
+#
+#     def ready(self):
+#         # Start scheduler only when server starts (not during migrations)
+#         from django.conf import settings
+#         if settings.DEBUG:
+#             from .scheduler import start_scheduler
+#             start_scheduler()
